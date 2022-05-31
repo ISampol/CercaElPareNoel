a4735ff259a16377a3ac54ce9086f73682796428
Ismael <ismaelsampol@gmail.com>
2022-05-31 16:14:52 +0200

    prueba

--- src/main/java/CercaEnPareNoel.java
+++ src/main/java/CercaEnPareNoel.java
@@ -4,20 +4,19 @@ public class CercaEnPareNoel {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String pareNoel = "*<]:-DOo";
-        String ren = ">:o)";
-        String follet = "<]:-D";
-
-        char trobador;
+        String[] elements = new String[]{"hola", "adeu", "hola2"};
         int comptador = 0;
 
-        String fotos = sc.nextLine();
+        System.out.println("Digues un text");
+        String text = sc.nextLine();
 
-        for (int i = 0; i < fotos.length(); i++) {
-
-            if (char)
-
+        for (int i = 0; i < text.length(); i++) {
+            if (text.contains(elements[i])){
+                comptador++;
+            }
         }
+
+        System.out.println(comptador);
 
         sc.close();
     }
